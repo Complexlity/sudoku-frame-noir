5b4f988ea96d246e43ea4e8de12be30c9e81104e
Complexlity <nwalozielijah@gmail.com>
2024-02-08 09:21:04 +0100

    feat: add frames.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,48 @@
-import Image from "next/image";
-import SudokuImage from '@/../sudokuImage'
+// import Image from "next/image";
+// import SudokuImage from '@/../sudokuImage'
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//      <SudokuImage />
+//     </main>
+//   );
+// }
+
+// ./app/page.tsx
+
+import {
+  FrameContainer,
+  FrameImage,
+  FrameButton,
+  useFramesReducer,
+  getPreviousFrame,
+  validateActionSignature,
+  FrameInput,
+} from "frames.js/next/server";
+
+//@ts-expect-error
+const reducer = (state, action) => ({ count: state.count + 1 });
+
+
+//@ts-expect-error
+export default async function Home(props) {
+  const previousFrame = getPreviousFrame(props.searchParams);
+  await validateActionSignature(previousFrame.postBody);
+  const [state, dispatch] = useFramesReducer(
+    reducer,
+    { count: 0 },
+    previousFrame
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <SudokuImage />
-    </main>
+    <FrameContainer
+      postUrl="/frames"
+      state={state}
+      previousFrame={previousFrame}
+    >
+      <FrameImage src="https://picsum.photos/seed/frames.js/1146/600" />
+      <FrameButton onClick={dispatch}>{state.count}</FrameButton>
+    </FrameContainer>
   );
 }
