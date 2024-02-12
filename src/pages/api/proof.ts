0344b0e31524c66b49725f3350752a51c07fe1d9
Complexlity <nwalozielijah@gmail.com>
2024-02-12 08:59:50 +0100

    feat: update proof api to accept post method

--- src/pages/api/proof.ts
+++ src/pages/api/proof.ts
@@ -5,22 +5,28 @@ import { Noir } from "@noir-lang/noir_js";
 import { NextApiRequest, NextApiResponse } from "next";
 
 
-export default async function handler(request: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let solution;
+  if (req.method === 'POST') {
+    solution = req.body.solution
+  }
+  else {
 
-  const puzzle=   [
+    const puzzle=   [
       0, 2, 0, 0, 5, 0, 0, 8, 9, 0, 5, 6, 7, 0, 9, 1, 0, 3, 7, 8, 9, 0, 2, 3, 0, 0,
       6, 2, 1, 0, 3, 0, 5, 0, 9, 0, 3, 6, 5, 8, 9, 7, 0, 1, 4, 8, 0, 7, 2, 0, 4, 3,
-  0, 5, 5, 3, 1, 6, 0, 0, 9, 7, 8, 6, 0, 2, 9, 0, 8, 0, 3, 1, 9, 7, 0, 5, 3, 1,
-  6, 4, 2,
-  ]
-
-  const solution = solveSudoku(puzzle)!
-
-
+      0, 5, 5, 3, 1, 6, 0, 0, 9, 7, 8, 6, 0, 2, 9, 0, 8, 0, 3, 1, 9, 7, 0, 5, 3, 1,
+      6, 4, 2,
+    ]
+    solution = solveSudoku(puzzle)!
+  }
 
 
-const inputsFail = { solution: puzzle};
+
+
+// const inputsFail = { solution: puzzle};
   const inputsCorrect = { solution };
+
 
 
   //@ts-expect-error
